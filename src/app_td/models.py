c9537336e8d7bc61bd097b31d29b16462d85cb8b
vverac <viverace@gmail.com>
2023-07-04 22:08:03 -0400

    impotacion de modelos a admin.py

--- src/app_td/models.py
+++ src/app_td/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.utils import timezone
 from django.db import models
-from django.urls import reverse
+
 
 #  se define una función que dvuelve la hora actual mas un delta de 7 dias
 def one_week():
     return timezone.now() + timezone.timedelta(days=7)
+
 #  defino 2 modelos:
+#  representa una lista de tareas
 class ToDoList(models.Model):
     title = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.title
 
-
+# representa un elemento pendidente
 class ToDoItem(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
@@ -23,7 +25,7 @@ class ToDoItem(models.Model):
 
 
     def __str__(self):
-        return f"{self.title}: due {self.due_date}"
+        return f"{self.title}: debe estar finalizada el dia {self.due_date}"
 
     # esta clase ordena segun la fecha de vencimiento
     class Meta:
